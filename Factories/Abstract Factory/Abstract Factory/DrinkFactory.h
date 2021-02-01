14f2e6b5942d504bf0eefc1666d47f200844450e
zpgy77@gmail.com <zpgy77@gmail.com>
2021-02-01 21:18:20 +0900

    Add funtional factory

--- Factories/Abstract Factory/Abstract Factory/DrinkFactory.h
+++ Factories/Abstract Factory/Abstract Factory/DrinkFactory.h
@@ -35,13 +35,19 @@ public:
   }
 };
 
+/**
+ * 함수형 팩토리
+ */
 class DrinkWithVolumeFactory
 {
+    //HotDrink: return, with no argument
   map<string, function<unique_ptr<HotDrink>()>> factories;
 public:
 
   DrinkWithVolumeFactory()
   {
+      //lamda factory method
+      //https://stackoverflow.com/questions/7627098/what-is-a-lambda-expression-in-c11
     factories["tea"] = [] {
       auto tea = make_unique<Tea>();
       tea->prepare(200);
@@ -54,5 +60,19 @@ public:
 
 inline unique_ptr<HotDrink> DrinkWithVolumeFactory::make_drink(const string& name)
 {
+    //함수를 invoke하기위해 ()
   return factories[name]();
 }
+
+/**
+ * 팩토리가 생성자보다 나은 점들
+ * (1) 팩토리는 객체의 생성을 거부할 수 있다.
+ * (2) 팩토리는 가독성 높은 명명이 가능하다.
+ * (3) 단일 팩토리가 서로 다른 여러 타입의 객체를 생성할 수 있다.
+ * (4) 팩토리에 다형성을 부여할 수 있다.
+ */
+
+/**
+ * 팩토리 VS 빌더
+ * 팩토리는 객체를 한번에 생성하지만, 빌더는 각 구성요소마다 필요한 정보를 제공하며 여러 단계를 거쳐 객체를 생성한다.
+ */
