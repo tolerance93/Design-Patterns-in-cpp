2a26bd487c9c693af1c8c4c311e7e2988dbddf4d
zpgy77@gmail.com <zpgy77@gmail.com>
2021-03-21 18:52:27 +0900

    Add Description

--- Behavioral Pattern/Chain of Responsibility/Broker Chain/cor_broker.cpp
+++ Behavioral Pattern/Chain of Responsibility/Broker Chain/cor_broker.cpp
@@ -4,7 +4,10 @@ using namespace std;
 #include <boost/signals2.hpp>
 using namespace boost::signals2;
 
-
+/**
+ * 커맨드 디자인 패턴. 크리처에 가해진 모든 변경 작업이 모두 완료되어 결괏값이 확정된 이후에 읽어야 한다.
+ * 어떤 크리처의 특정 상태 값에 대한 조회라는 개념을 캡슐화하고 있다.
+ */
 struct Query
 {
   string creature_name;
@@ -19,10 +22,14 @@ struct Query
   {
   }
 };
-
-struct Game // mediator Design Pattern
+/**
+ * [이벤트 브로커]
+ * 중앙 집중화된 컴포넌트. 게임에서 발생할 수 있는 모든 변경 작업의 목록을 관리하고 특정 크리처의 공격력 또는 방어력의 상태를
+ * 그간의 변경 작업 이력이 모두 반영된 상태로 구할 수 있게 한다.
+ */
+struct Game // mediator & observer
 {
-  signal<void(Query&)> queries;
+  signal<void(Query&)> queries; // 부스트 라이브러리. 어떤 신호를 발생시키고 그 신호를 기다리고 있는 몯느 수신처가 신호를 처리할 수 있게 한다.
 };
 
 class Creature
@@ -74,6 +81,10 @@ public:
 
 class DoubleAttackModifier : public CreatureModifier
 {
+    /**
+     * 나중에 객체가 소멸되었을 때 이벤트의 연결을 해제할 수 있도록 연결 정보도 저장해야 한다.
+     * 변경 작업이 임시적으로 적용되고 유효한 조건을 벗어났을 때 더 이상 적용되지 않게 할 수 있다.
+     */
   connection conn;
 public:
   DoubleAttackModifier(Game& game, Creature& creature)
