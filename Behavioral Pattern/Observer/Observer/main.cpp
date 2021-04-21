f2c5be47cd35214a3c4d5eb56e2e4d4f5964dc3e
zpgy77@gmail.com <zpgy77@gmail.com>
2021-04-21 21:30:14 +0900

    Observer Design Pattern with boost signal

--- Behavioral Pattern/Observer/Observer/main.cpp
+++ Behavioral Pattern/Observer/Observer/main.cpp
@@ -1,8 +1,10 @@
 #include "Observer.h"
 #include <iostream>
 #include "Observable.h"
+#include <boost/signals2.hpp>
 
 using namespace std;
+using namespace boost;
 
 class Person : public Observable<Person>// observable
 {
@@ -36,9 +38,32 @@ private:
     }
 };
 
+template <typename T> struct Observable2
+{
+    signals2::signal<void(T&, const string&)> field_changed;
+};
+
+class Person2 : public Observable2<Person2>
+{
+    int age;
+public:
+    int get_age() const
+    {
+        return age;
+    }
+    
+    void set_age(int age)
+    {
+        if (this->age == age) return;
+        this->age = age;
+        field_changed(*this, "age");
+    }
+};
+
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
+    /*
     Person person{10};
     //실제 동작을 수행함: 결과 프린트
     ConsolePersonObserver cpo;
@@ -51,6 +76,19 @@ int main(int argc, const char * argv[]) {
     
     person.unsubscribe(cpo);
     person.set_age(13);
+     */
+    
+    //boost
+    Person2 p2;
+    //subscribe boost signal은 옵저버가 따로 필요없다. 람다함수 이용
+    auto conn = p2.field_changed.connect(
+      [](Person2& p, const string& field_name)
+      {
+        cout << field_name << " has changed\n";
+      }
+    );
+    p2.set_age(20);
+    conn.disconnect(); // unsubscribe;
     
     return 0;
 }
